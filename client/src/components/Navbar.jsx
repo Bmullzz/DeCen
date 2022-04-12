112bf73085f82312f5a13073c58198184bef4cfb
Brian Mullin <brmullin@email.msmary.edu>
2022-04-12 14:36:43 -0400

    fixed an issue with syntax in the Navbar component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ import logo from '../../images/logo.png';
 
 const NavbarItem = ({ title, classProps }) => {
     return (
-        <li className={'mx-4 cursor-pointer ${classProps}'}>
+        <li className={`mx-4 cursor-pointer ${classProps}`}>
             {title}
         </li>
     )
