f7406dd665f237316bfbb9a475c273838d22c0c5
Brian Mullin <brmullin@email.msmary.edu>
2022-08-16 16:10:32 -0400

    added logic to check if wallet is connect

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -20,6 +20,19 @@ const getEthereumContract = () => {
 }
 
 export const TransactionProvider = ({ children }) => {
+
+    const checkIfWalletIsConnected = async () => {
+        if(!ethereum) return alert("Please Install Metamask");
+
+        const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+        console.log(accounts);
+    }
+
+    useEffect(() => {
+        checkIfWalletIsConnected();
+    }, []);
+
     return (
         <TransactionContext.Provider value={{ value: 'test' }}>
             {children}
