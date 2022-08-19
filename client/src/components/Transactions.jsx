32e9f56f1b9277aea14ff20393dd1c5306a9c09e
Brian Mullin <brmullin@email.msmary.edu>
2022-08-19 17:17:01 -0400

    added transaction list function

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -3,10 +3,15 @@ import React, { useContext } from 'react';
 import { TransactionContext } from '../context/TransactionContext';
 
 import dummyData from '../utils/dummyData';
+import { shortenAddress } from '../utils/shortenAddress';
+
+const TransactionCard = ({addressTo, addressFrom, tiemstamp, message, keyword, amount, url}) => {
+    return 'transaction';
+}
 
 const Transactions = () => {
     const { currentAccount } = useContext(TransactionContext);
-
+    
     return (
         <div className="flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
             <div className="flex flex-col md:p-12 py-12 px-4">
@@ -15,6 +20,12 @@ const Transactions = () => {
                 ) : (
                     <h3 className="text-white text-3xl text-center my-2">Connect your Account to see the latest transactions</h3>
                 )}
+
+                <div className="flex flex-wrap justify-center items-center mt-10">
+                    {dummyData.reverse().map((transaction, i) => (
+                        <TransactionCard key={i} {...transaction}/>
+                    ))}
+                </div>
             </div>
         </div>
     );
